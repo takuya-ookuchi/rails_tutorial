74a03231024ddccb222636c21dbac9eff19bb4f6
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-11 14:28:35 +0900

    編集アクションを追加
    編集アクションを実装（修正）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,15 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  def edit; end
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました"
+  end
 
   def create
     task = Task.new(task_params)
