45a1b55f111fcbb9a2950934738de2d5ff20734f
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-11 09:49:24 +0900

    新規登録機能を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,22 @@
 class TasksController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
   def new
+    @task = Task.new
   end
 
-  def edit
+  def edit; end
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
