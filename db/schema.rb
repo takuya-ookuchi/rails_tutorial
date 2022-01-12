ad3a2572ef899344a809f2df1e5d0fe868a858ce
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-12 14:55:54 +0900

    名前を30字以内に制限

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_004526) do
+ActiveRecord::Schema.define(version: 2022_01_12_055059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
