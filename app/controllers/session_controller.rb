8a7b97c176f02dbb27900b2e521986548f0f7361
大口拓哉 <ookuchitakuya@MacBook-Pro-4.local>
2022-01-13 15:11:57 +0900

    修正　キーの指定が間違っていた

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,7 +3,7 @@ class SessionController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: session_params[email])
+    user = User.find_by(email: session_params[:email])
 
     if user&.authenticate(session_params[:password])
       sesson[:user_id] = user.id
